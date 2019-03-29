048c8f3ba1d0f2d81d0bff350366bd752e955fe8
n60512 <n60512@gmail.com>
2019-03-29 13:48:33 +0800

    20190329 update

--- app/src/main/java/com/example/yafun/thinkingapplication/LoginActivity.java
+++ app/src/main/java/com/example/yafun/thinkingapplication/LoginActivity.java
@@ -1,12 +1,9 @@
 package com.example.yafun.thinkingapplication;
 
 import android.content.Intent;
-import android.graphics.Bitmap;
-import android.graphics.BitmapFactory;
 import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.util.Base64;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
@@ -14,15 +11,21 @@ import android.widget.EditText;
 import android.widget.Toast;
 import android.os.AsyncTask;
 import android.content.SharedPreferences;
+import android.view.WindowManager;
+import android.util.DisplayMetrics;
+import android.widget.TextView;
+import android.graphics.Point;
+import android.support.constraint.ConstraintLayout.LayoutParams;
+import android.content.Context;
 
 import org.json.*;
-import java.util.Map;
 import java.util.Iterator;
 
 
 public class LoginActivity extends AppCompatActivity {
 
     // declare variable
+    private TextView TextView2,TextView3;
     private EditText edtId, edtPwd;
     private Button btnSignIn;
     private Button btnSignUp;
@@ -34,6 +37,22 @@ public class LoginActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        getWindow().clearFlags(WindowManager.LayoutParams.FLAG_FORCE_NOT_FULLSCREEN);
+        getWindow().addFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN);
+
+        // 全屏幕模式
+        View decorView = getWindow().getDecorView();
+        int uiOptions = View.SYSTEM_UI_FLAG_LAYOUT_STABLE
+                | View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
+                | View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
+                | View.SYSTEM_UI_FLAG_HIDE_NAVIGATION // hide nav bar
+//                    | View.SYSTEM_UI_FLAG_FULLSCREEN // hide status bar
+                | View.SYSTEM_UI_FLAG_IMMERSIVE;
+        decorView.setSystemUiVisibility(uiOptions);
+        getWindow().addFlags(WindowManager.LayoutParams.FLAG_TRANSLUCENT_NAVIGATION);
+
+        new SettingLayout().execute();
 
         // set variable value
         edtId = (EditText) findViewById((R.id.edtId));
@@ -73,6 +92,131 @@ public class LoginActivity extends AppCompatActivity {
             }
         });
     }
+    public static float convertDpToPixel(float dp, Context context){
+        return dp * ((float) context.getResources().getDisplayMetrics().densityDpi / DisplayMetrics.DENSITY_DEFAULT);
+    }
+    public static float convertPixelsToDp(float px, Context context){
+        return px / ((float) context.getResources().getDisplayMetrics().densityDpi / DisplayMetrics.DENSITY_DEFAULT);
+    }
+    public static float getDPI(Context context){
+        return ((float) context.getResources().getDisplayMetrics().densityDpi);
+    }
+
+    private class SettingLayout extends AsyncTask<String, Void, String> {
+        @Override
+        protected String doInBackground(String... params) {
+
+            return null;
+        }
+
+        @Override
+        protected void onPostExecute(String result) {
+        }
+
+        @Override
+        protected void onPreExecute() {
+            final  Point size = new Point();
+            getWindowManager().getDefaultDisplay().getRealSize(size);
+            Log.d("手機尺寸為", size.x+"X"+size.y);
+
+            TextView2 = (TextView)findViewById(R.id.textView2);
+            TextView3 = (TextView)findViewById(R.id.textView3);
+            edtId = (EditText) findViewById((R.id.edtId));
+            edtPwd = (EditText) findViewById((R.id.edtPwd));
+            btnSignIn = (Button) findViewById(R.id.btnSignIn);
+            btnSignUp = (Button) findViewById(R.id.btnSignUp);
+
+            final LayoutParams lp_tv2 = (LayoutParams) TextView2.getLayoutParams();
+            final LayoutParams lp_tv3 = (LayoutParams) TextView3.getLayoutParams();
+            final LayoutParams lp_btSig = (LayoutParams) btnSignIn.getLayoutParams();
+            final LayoutParams lp_btSup = (LayoutParams) btnSignUp.getLayoutParams();
+            final LayoutParams lp_edtId = (LayoutParams) edtId.getLayoutParams();
+            final LayoutParams lp_edPwd = (LayoutParams) edtPwd.getLayoutParams();
+
+            // 此設 px
+            lp_tv2.topMargin = Math.round(
+                    getTransformPx(convertPixelsToDp(lp_tv2.topMargin,getApplicationContext()),size.x)
+            );
+            lp_tv3.topMargin = Math.round(
+                    getTransformPx(convertPixelsToDp(lp_tv3.topMargin,getApplicationContext()),size.x)
+            );
+            lp_btSig.topMargin = Math.round(
+                    getTransformPx(convertPixelsToDp(lp_btSig.topMargin,getApplicationContext()),size.x)
+            );
+            lp_btSup.topMargin = Math.round(
+                    getTransformPx(convertPixelsToDp(lp_btSup.topMargin,getApplicationContext()),size.x)
+            );
+            lp_edtId.topMargin = Math.round(
+                    getTransformPx(convertPixelsToDp(lp_edtId.topMargin,getApplicationContext()),size.x)
+            );
+            lp_edPwd.topMargin = Math.round(
+                    getTransformPx(convertPixelsToDp(lp_edPwd.topMargin,getApplicationContext()),size.x)
+            );
+
+            // 此是設dp
+            TextView2.setTextSize(Math.round(
+                    convertPixelsToDp(
+                            getTransformPx(convertPixelsToDp(TextView2.getTextSize(),getApplicationContext()),size.x)
+                            ,getApplicationContext())
+            ));
+            TextView3.setTextSize(Math.round(
+                    convertPixelsToDp(
+                            getTransformPx(convertPixelsToDp(TextView3.getTextSize(),getApplicationContext()),size.x)
+                            ,getApplicationContext())
+            ));
+            edtId.setTextSize(Math.round(
+                    convertPixelsToDp(
+                            getTransformPx(convertPixelsToDp(edtId.getTextSize(),getApplicationContext()),size.x)
+                            ,getApplicationContext())
+            ));
+            edtPwd.setTextSize(Math.round(
+                    convertPixelsToDp(
+                            getTransformPx(convertPixelsToDp(edtPwd.getTextSize(),getApplicationContext()),size.x)
+                            ,getApplicationContext())
+            ));
+            btnSignUp.setTextSize(Math.round(
+                    convertPixelsToDp(
+                            getTransformPx(convertPixelsToDp(btnSignUp.getTextSize(),getApplicationContext()),size.x)
+                            ,getApplicationContext())
+            ));
+            btnSignIn.setTextSize(Math.round(
+                    convertPixelsToDp(
+                            getTransformPx(convertPixelsToDp(btnSignIn.getTextSize(),getApplicationContext()),size.x)
+                            ,getApplicationContext())
+            ));
+
+
+
+            TextView2.setLayoutParams(lp_tv2);
+            TextView3.setLayoutParams(lp_tv3);
+            edtId.setLayoutParams(lp_edtId);
+            edtPwd.setLayoutParams(lp_edPwd);
+            btnSignUp.setLayoutParams(lp_btSup);
+            btnSignIn.setLayoutParams(lp_btSig);
+        }
+
+        @Override
+        protected void onProgressUpdate(Void... values) {
+        }
+
+        private float getTransformPx(float currentDP,float screenWidth){
+            // 傳入當前物件DP與螢幕寬，回傳當前屏幕比的 DP 轉 PX
+            float rate = ((float)screenWidth/(float)1080);
+            float Px_of_1080 = currentDP * (float)(420.0/160.0);
+            float Px_of_Current = Px_of_1080*rate;
+
+            Log.d("rate",Float.toString((float)screenWidth/(float)1080));
+            Log.d("Px_of_1080",Float.toString(currentDP * (float)(420.0/160.0)));
+            Log.d("Px_of_Current",Float.toString(Px_of_Current));
+
+            //(float)(393.75* ((float)screenWidth/(float)1080))
+            return Px_of_Current;
+
+        }
+    }
+
+
+
 
     /**
      * AsyncLogin Class
@@ -88,38 +232,8 @@ public class LoginActivity extends AppCompatActivity {
             // 設定 permission;確認登入
             permission = conn.checkLogin();
 
-            //1003 load data
-            /*connRecord = new ConnServer();//get Record
-            String[] tmparr = connRecord.PersonalRecord("drawing",params[0]);
-            imagetest = getSharedPreferences("drawing_record", MODE_PRIVATE);
-
-            for(int i = 0; i < tmparr.length; i++) {
-                try{
-                    JSONArray data = new JSONArray(tmparr[i]);
-                    //Log.d("JSONArray",data.get(1).toString());//data
-
-                    Log.d("JSONArray",data.get(0).toString());
-                    imagetest.edit()
-                            .putString(data.get(0).toString(), data.get(1).toString())
-                            .commit();
-
-                }catch (org.json.JSONException e){
-                    Log.d("Err","");
-                }
-            }*/
-
-
-
             writeShardPreferences("drawing",params[0]);
             writeShardPreferences("drawingmult",params[0]);
-
-            /*
-            Map<String,?> keys = imagetest.getAll();
-
-            for(Map.Entry<String,?> entry : keys.entrySet()){
-                Log.d("map values",entry.getKey() + ": " +
-                        entry.getValue().toString());
-            }*/
 
             return null;
         }
@@ -178,6 +292,7 @@ public class LoginActivity extends AppCompatActivity {
 
         @Override
         protected void onPreExecute() {
+
         }
 
         @Override
